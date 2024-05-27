82c9a008f095386ca82e44ee16d0f32a59037575
Khisab Kurniawan <khisabkurniawan@outlook.com>
2024-05-27 11:38:55 +0700

    Update theme.config.jsx

--- theme.config.jsx
+++ theme.config.jsx
@@ -10,7 +10,7 @@ const config = {
     const { asPath } = useRouter()
     if (asPath !== '/') {
       return {
-        titleTemplate: '%s – Reforged Docs'
+        titleTemplate: '%s – Reforged Public API Reference'
       }
     }
   },
@@ -20,7 +20,7 @@ const config = {
         src="https://cdn.discordapp.com/attachments/1240631069879963649/1240631093846081689/reforged.png?ex=6647433e&is=6645f1be&hm=4838d2e651bc6c8648c481d40ba7f05bde9f1b66f786cd357bb5cc41abd3a62c&"
         width={24} height={24} alt="Logo Reforged"
       />
-      <b style={{ marginLeft: '.4em' }}>Reforged Docs</b>
+      <b style={{ marginLeft: '.4em' }}>Reforged Public API Reference</b>
     </>
   ),
   chat: {
